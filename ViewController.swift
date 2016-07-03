ec6dffb6d6c7141a5365c3020eb5484db25f1a94
isoerdem <isoerdem@isoerdems-mac.local>
2016-07-03 22:33:10 +0300

    storyboardfix

--- ViewController.swift
+++ ViewController.swift
@@ -14,16 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var uncoolButton: UIButton!
     @IBOutlet weak var txtLabel: UILabel!
     
-    var phrases = ["Booting from floopy .........\n","Reading from disk .........\n","Updating registry ............ \n","...........","Welcome Norton, \n It is nice to see you!"]
+    var phrases = ["Booting from floopy .........\n","Reading from disk .........\n","Updating registry ............\n","...........","Welcome Norton, \n It is nice to see you!"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
        
-        /*
+        
         txtLabel.text = ""
-        for var text = 0; text < phrases.count; text++ {
+        /*
+        for var text = 0; text < phrases.count; text+	+ {
             var txt = txtLabel.text!  // Defining a value for the first time
             txt += phrases[text]
             txtLabel.text = txt
