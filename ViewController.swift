d8de820597d77af83b26a26a4294950315b4e0c2
isoerdem <isoerdem@isoerdems-mac.local>
2016-07-03 21:25:33 +0300

    Added an array to show a text on the main storyboard

--- ViewController.swift
+++ ViewController.swift
@@ -12,11 +12,28 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var coolLogo: UIImageView!
     @IBOutlet weak var uncoolButton: UIButton!
+    @IBOutlet weak var txtLabel: UILabel!
+    
+    var phrases = ["Booting from floopy .........\n","Reading from disk .........\n","Updating registry ............ \n","...........","Welcome Norton, \n It is nice to see you!"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        /*
+        txtLabel.text = ""
+        for var text = 0; text < phrases.count; text++ {
+            var txt = txtLabel.text!  // Defining a value for the first time
+            txt += phrases[text]
+            txtLabel.text = txt
+        }
+        */
+        for phrase in phrases {
+            var txt = txtLabel.text!  // Defining a value for the first time
+            txt += phrase
+            txtLabel.text = txt
+        }
     }
 
     override func didReceiveMemoryWarning() {
